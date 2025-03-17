4c54ee6646ab0eb42b4b552a1522eaa4095dc758
xxzn <mrq0218@163.com>
2025-03-17 11:45:41 +0800

    添加RNN从零开始的模型类

--- rnn_train.py
+++ rnn_train.py
@@ -1,7 +1,24 @@
 import math
 import torch
 from torch import nn
+from torch.nn import functional as F
 from d2l import torch as d2l
+
+class RNNModelScratch:
+    """从零开始实现的循环神经网络模型"""
+    def __init__(self, vocab_size, num_hiddens, device, 
+                 get_params, init_state, forward_fn):
+        self.vocab_size, self.num_hiddens = vocab_size, num_hiddens
+        self.params = get_params(vocab_size, num_hiddens, device)
+        self.inin_state, self.forward_fn = init_state, forward_fn
+
+    def __call__(self, X, state):
+        # X.T是因为X原本的形状是（批量大小， 时间步数量）
+        X = F.one_hot(X.T, self.vocab_size).type(torch.float32)
+        return self.forward_fn(X, state, self.params)
+    
+    def begin_state(self, batch_size, device):
+        return self.inin_state(batch_size, self.num_hiddens, device)
 
 def predict_ch8(prefix, num_preds, net, vocab, device):
     """在prefix后面生成新字符"""
